0d9c7c3ae66b69a96292b388f27865186c2d5395
teofanescumisa <56659788+teofanescumisa@users.noreply.github.com>
2019-10-21 20:53:15 +0300

    CleanUp, add antd

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
+import { Layout , Typography , Empty} from 'antd';
+import 'antd/dist/antd.css';
 import './App.css';
 
+
+const { Header, Content ,Footer} = Layout;
+const { Title } = Typography;
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Layout>
+     <Header>
+      <Title>
+        Movie List Application !
+      </Title>
+     </Header>
+     <Content>
+       <Empty></Empty>
+     </Content>
+     <Footer>
+       Footer
+     </Footer>
+    </Layout>
   );
 }
 
