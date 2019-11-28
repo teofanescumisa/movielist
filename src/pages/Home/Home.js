dec8aa30a7f889782fe1c2c82166dd636ef4d208
teofanescumisa <56659788+teofanescumisa@users.noreply.github.com>
2019-11-28 16:48:45 +0200

    delete button

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,18 +1,16 @@
-import React, { Component, Fragment } from "react";
-import { Row, Col, Input, Button } from "antd";
-import axios from "axios";
-import Settings from "../../config";
-import { ResultList } from "./ResultList/ResultList";
-import MovieList from "./MovieList/MovieList";
-
+import React, { Component, Fragment } from 'react'
+import { Row, Col, Input, Button } from 'antd'
+import axios from 'axios'
+import Settings from '../../config'
+import { ResultList } from './ResultList/ResultList'
+import MovieList from './MovieList/MovieList'
 export class Home extends Component {
   state = {
-    searchTerm: "",
+    searchTerm: '',
     results: [],
     isLoading: false,
-    savedMovies: []
-  };
-
+    savedMovies: [],
+  }
   componentDidMount() {
     const savedMovies = localStorage.getItem('savedMovies')
     if (savedMovies) {
@@ -21,42 +19,50 @@ export class Home extends Component {
       })
     }
   }
-
   handleSearchChange = event => {
-    const { value } = event.target;
+    const { value } = event.target
     if (value.length === 0) {
-      this.setState({ results: [] });
+      this.setState({ results: [] })
     }
     this.setState({
-      searchTerm: value
-    });
-  };
-
+      searchTerm: value,
+    })
+  }
   handleSearchClick = event => {
-    event.preventDefault();
-    const url = `https://api.themoviedb.org/3/search/movie?api_key=${Settings.APIKEY}&query=${this.state.searchTerm}`;
-    this.setState({ isLoading: true });
+    event.preventDefault()
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=${Settings.APIKEY}&query=${this.state.searchTerm}`
+    this.setState({ isLoading: true })
     axios.get(url).then(response => {
       this.setState({
         results: response.data.results,
-        isLoading: false
-      });
-    });
-  };
-
+        isLoading: false,
+      })
+    })
+  }
   handleAddMovie = movie => {
-    const { savedMovies } = this.state;
-    savedMovies.push(movie);
+    const { savedMovies } = this.state
+    savedMovies.push(movie)
     this.setState({
       savedMovies: savedMovies,
       results: []
-    });
-    localStorage.setItem("savedMovies", JSON.stringify(this.state.savedMovies));
-  };
+    }, () => {
+      localStorage.setItem('savedMovies', JSON.stringify(this.state.savedMovies))
+    })
+  }
+
+  handeDelete = movie => {
+    const { savedMovies } = this.state;
+    const movies = savedMovies.filter(item => item.id !== movie.id);
+
+    this.setState({
+      savedMovies: movies
+    }, () => {
+      localStorage.setItem('savedMovies', JSON.stringify(this.state.savedMovies))
+    })
+  }
 
   render = () => {
-    const { searchTerm, isLoading } = this.state;
-    console.log("render called");
+    const { searchTerm, isLoading } = this.state
     return (
       <Fragment>
         <Row>
@@ -76,8 +82,7 @@ export class Home extends Component {
                 icon="search"
                 disabled={searchTerm.length < 2}
                 loading={isLoading}
-                onClick={this.handleSearchClick}
-              >
+                onClick={this.handleSearchClick}>
                 Search
               </Button>
             </Col>
@@ -87,6 +92,7 @@ export class Home extends Component {
           <Col span={10} offset={6}>
             <ResultList
               results={this.state.results}
+              savedMovies={this.state.savedMovies}
               onAddMovie={this.handleAddMovie}
             />
           </Col>
@@ -94,11 +100,14 @@ export class Home extends Component {
         <Row>
           <Col span={20} offset={2} className="movie_list">
             {this.state.savedMovies.length > 0 && (
-              <MovieList movies={this.state.savedMovies} />
-            )}
-          </Col>
-        </Row>
-      </Fragment>
-    );
-  };
+              <MovieList
+              movies={this.state.savedMovies}
+              handleDelete={this.handeDelete}
+            />
+          )}
+        </Col>
+      </Row>
+    </Fragment>
+  )
+}
 }
